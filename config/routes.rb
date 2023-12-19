df997a88f52830519cfe70c9cd2f588c55cd782e
Gian Cunningham <gian@cunninghams.ch>
2023-12-19 11:58:39 +0100

    all controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,19 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
+  resources :bills
+  resources :clients
+  resources :users
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  devise_scope :user do
+    authenticated :user do
+      root 'bills#index', as: :authenticated_root
+    end
+
+    # Redirect users to login page if not authenticated
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
