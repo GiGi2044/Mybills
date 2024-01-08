9ab88d981ed7f3080b4a0477bcadb95d1d43b4a1
Gian Cunningham <gian@cunninghams.ch>
2024-01-08 09:55:58 +0100

    Still perfecting bill pdf

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -102,12 +102,13 @@ class BillsController < ApplicationController
     initialmove_y = 5
     address_x = 5
     invoice_header_x = 325
-    lineheight_y = 12
+    lineheight_y = 18
     font_size = 9
 
     business_name = user.business_name
     business_address = user.street
     business_location = user.city
+    business_phone = user.phone_number
 
     invoice_number = bill.id
     invoice_date = bill.bill_date
@@ -149,15 +150,16 @@ class BillsController < ApplicationController
     pdf.move_down initialmove_y
     # user details
     pdf.text_box business_name, :at => [address_x,  pdf.cursor], size: 20, style: :bold, color: :"1C6DD0"
-    pdf.move_down 27
+    pdf.move_down 40
     pdf.text_box business_address, :at => [address_x,  pdf.cursor]
     pdf.move_down lineheight_y
     pdf.text_box business_location, :at => [address_x,  pdf.cursor]
     pdf.move_down lineheight_y
+    pdf.text_box "Phone: #{business_phone}", :at => [address_x,  pdf.cursor]
 
     last_measured_y = pdf.cursor
 
-    pdf.move_up 55
+    pdf.move_up 80
 
     invoice_header_data = [
       ["INVOICE"],
@@ -171,9 +173,10 @@ class BillsController < ApplicationController
 
     pdf.move_cursor_to pdf.bounds.height
 
+
     # pdf.text_box logopath, :width => 215, :position => :right
     pdf.move_cursor_to last_measured_y
-    pdf.move_down 10
+    pdf.move_up 10
 
     invoice_header_data = [
       ["Invoice #", "Date"],
@@ -197,7 +200,7 @@ class BillsController < ApplicationController
     pdf.text_box bank_bic, at: [bank_details3_x, pdf.cursor]
     pdf.text_box "ACCT Nr.", at: [bank_details2_x_title, pdf.cursor]
     pdf.text_box bank_account_number, at: [bank_details2_x, pdf.cursor]
-    pdf.move_down 45
+    pdf.move_down 35
     last_measured_y = pdf.cursor
 
     invoice_header_data = [
@@ -211,21 +214,22 @@ class BillsController < ApplicationController
       style(row(0..1).columns(0..1), :align => :left)
     end
 
+    client_lineheight_y = 12
     client_details_style = { size: 10 }
     pdf.move_down 5
-    pdf.text_box client_name, at: [address_x, pdf.cursor], **client_details_style
-    pdf.move_down lineheight_y
     pdf.text_box client_contact_name, at: [address_x, pdf.cursor], **client_details_style
-    pdf.move_down lineheight_y
+    pdf.move_down client_lineheight_y
+    pdf.text_box client_name, at: [address_x, pdf.cursor], **client_details_style
+    pdf.move_down client_lineheight_y
     pdf.text_box client_address, at: [address_x, pdf.cursor], **client_details_style
-    pdf.move_down lineheight_y
+    pdf.move_down client_lineheight_y
     pdf.text_box client_city, at: [address_x, pdf.cursor], **client_details_style
 
     pdf.move_cursor_to last_measured_y
 
     invoice_header_data = [
       ["Customer ID", "TERMS"],
-      [customer_id, customer_terms],
+      [customer_id, "#{customer_terms} days"],
     ]
 
       pdf.table(invoice_header_data, :position => invoice_header_x, :width => 215) do
@@ -235,7 +239,7 @@ class BillsController < ApplicationController
       style(row(0..1).columns(0..1), :align => :center) # Center the second row horizontally
     end
 
-    pdf.move_down 75
+    pdf.move_down 70
 
     invoice_services_data = [
       ["Description", "Days", "Daily rate", "Amount"],
@@ -253,7 +257,7 @@ class BillsController < ApplicationController
       style(row(0).columns(-1), :borders => [:top, :right, :bottom])
       style(row(-1), :border_width => 2)
       style(column(2..-1), :align => :right)
-      style(columns(0), :width => 275)
+      style(columns(0), :width => 255)
       style(columns(1), :width => 75)
 
       # Calculate the height of a single row based on the content in the first row
@@ -265,8 +269,9 @@ class BillsController < ApplicationController
       end
     end
 
+    thanks_address_x = 90
     pdf.move_down 10
-    pdf.text_box "Thank you for your business!", at: [address_x, pdf.cursor]
+    pdf.text_box "Thank you for your business!", at: [thanks_address_x, pdf.cursor]
     pdf.move_up 9
     invoice_services_totals_data = [
       ["Subtotal", bill_amount],
@@ -287,7 +292,7 @@ class BillsController < ApplicationController
 
     pdf.text_box contact_text, at: [x_position, pdf.cursor]
     pdf.move_down lineheight_y
-    pdf.text_box contact_details, at: [x1_position, pdf.cursor]
+    pdf.text_box contact_details, at: [x1_position, pdf.cursor], style: :bold
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
   has_many :clients
   has_many :bills
   has_many :services
+
+  validates :fullname, presence: true
+  validates :business_name, presence: true
+  validates :email, presence: true
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :bank_name, presence: true
+  validates :iban, presence: true
+  validates :bic, presence: true
+  validates :account_number, presence: true
+  validates :phone_number, presence: true
 end
