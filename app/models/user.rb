903c69874a7923f878a04868de3aeb0500d1dc1d
Gian Cunningham <gian@cunninghams.ch>
2024-01-08 10:44:56 +0100

    validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,21 +5,4 @@ class User < ApplicationRecord
   has_many :clients
   has_many :bills
   has_many :services
-
-  validates :fullname, presence: true, if: :signed_up?
-  validates :business_name, presence: true, if: :signed_up?
-  validates :email, presence: true, if: :signed_up?
-  validates :street, presence: true, if: :signed_up?
-  validates :city, presence: true, if: :signed_up?
-  validates :bank_name, presence: true, if: :signed_up?
-  validates :iban, presence: true, if: :signed_up?
-  validates :bic, presence: true,if: :signed_up?
-  validates :account_number, presence: true, if: :signed_up?
-  validates :phone_number, presence: true, if: :signed_up?
-
-  private
-
-  def signed_up?
-    self.is_signed_up
-  end
 end
