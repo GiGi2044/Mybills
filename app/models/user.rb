47f1ab95d86c2d1b726fdcb3d98b961696831e6a
Gian Cunningham <gian@cunninghams.ch>
2024-01-15 14:15:31 +0100

    implementing google login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
-  :validatable
+  :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
   has_many :clients
   has_many :bills
   has_many :services
 
   private
 
-  def self.from_google(u)
-    create_with(uid: u[:uid], provider: 'google',
-                password: Devise.friendly_token[0, 20]).find_or_create_by!(email: u[:email])
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token(length=20)
+    end
   end
 
 end
