7910183b7ae0a47cc3dba22d874c05aed0e8d1ab
Gian Cunningham <gian@cunninghams.ch>
2024-01-16 21:08:40 +0100

    everything looking better but redirecting in omniauth still not working

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,18 +2,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     user = User.from_google(from_google_params)
 
+    Rails.logger.debug "User found: #{user.inspect}" # Debug output
+
     if user.present?
       sign_out_all_scopes
       flash[:notice] = t 'devise.omniauth_callbacks.success', kind: 'Google'
 
-      # Check if it's the user's first sign in
-      if user.last_sign_in_at.nil?
-        # Redirect to a specific path on first sign in
-        redirect_path = first_time_sign_in_path
-      else
-        # Redirect to a different path on subsequent sign ins
-        redirect_path = subsequent_sign_in_path
-      end
+      redirect_path = user.last_sign_in_at.nil? ? first_time_sign_in_path : subsequent_sign_in_path
+      Rails.logger.debug "Redirect path: #{redirect_path}" # Debug output
 
       sign_in_and_redirect user, event: :authentication, location: redirect_path
     else
