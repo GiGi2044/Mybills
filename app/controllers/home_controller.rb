1936a5fee8ef97dd7e80c31a235cc244fc4ac239
Gian Cunningham <gian@cunninghams.ch>
2024-01-18 21:28:39 +0100

    a

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
     @plans = Plan.all
+    @bills = current_user.bills
+    @bills_count = @bills.count
+    @unsent_bills_count = @bills.where(status: 'unsent').count
+    @sent_bills_count = @bills.where(status: 'sent').count
+    @paid_bills_count = @bills.where(status: 'paid').count
   end
 
   def show
