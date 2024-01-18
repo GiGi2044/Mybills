4884b23195e03e58bbcb3b19b5c21fd519c532ae
Gian Cunningham <gian@cunninghams.ch>
2024-01-18 21:56:56 +0100

    logged homepage stuff, first image just for example for now

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,16 @@
 class HomeController < ApplicationController
   def index
     @plans = Plan.all
-    @bills = current_user.bills
-    @bills_count = @bills.count
-    @unsent_bills_count = @bills.where(status: 'unsent').count
-    @sent_bills_count = @bills.where(status: 'sent').count
-    @paid_bills_count = @bills.where(status: 'paid').count
+    if user_signed_in?
+      @plans = Plan.all
+      @bills = current_user.bills
+      @bills_count = @bills.count
+      @unsent_bills_count = @bills.where(status: 'unsent').count
+      @sent_bills_count = @bills.where(status: 'sent').count
+      @paid_bills_count = @bills.where(status: 'paid').count
+    else
+      @plans = Plan.all
+    end
   end
 
   def show
