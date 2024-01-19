1c213662e9b943674938a4d16d5e1a69d7bebab1
Gian Cunningham <gian@cunninghams.ch>
2024-01-19 09:50:32 +0100

    SEO details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
 
+  def default_url_options
+  { host: ENV["justinvoice.it"] || "localhost:3000" }
+  end
+
   private
   def after_sign_in_path_for(resource)
     # Check if it's the user's first sign in
